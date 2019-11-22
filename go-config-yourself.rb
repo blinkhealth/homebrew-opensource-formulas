10965f3bc0c8ea3e73414b2ceede54e55aaa2df5
Roberto Hidalgo <rob@blinkhealth.com>
2019-11-22 09:49:44 -0500

    fix download url

--- go-config-yourself.rb
+++ go-config-yourself.rb
@@ -4,7 +4,7 @@ require_relative 'lib/github-release'
 class GoConfigYourself < Formula
   desc 'Want to store secrets in your repo? go config yourself!'
   homepage 'https://github.com/blinkhealth/go-config-yourself'
-  url 'https://github.com/blinkhealth/go-config-yourself/releases/download/v0.0.0/gcy-macos-amd64.tgz'
+  url 'https://github.com/blinkhealth/go-config-yourself/releases/download/v1.0.0/gcy-macos-amd64.tgz'
   sha256 '528c1680e2dd03a7b1ea0f56727622a57726dd1db23f19f334e5325aba505e2d'
   head 'https://github.com/blinkhealth/go-config-yourself', using: GithubPreReleaseDownloadStrategy
 
